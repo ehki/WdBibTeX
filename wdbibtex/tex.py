537035ecb5621297a8ab17f9a9278f18d08329db
ehki <0y35.ejiri.vmqewyhw@gmail.com>
2022-02-19 17:24:02 +0900

    Add bibtex function

--- wdbibtex/tex.py
+++ wdbibtex/tex.py
@@ -1,7 +1,6 @@
 import pathlib
 import os
 
-
 defaultpreamble = (
     '\\documentclass[10pt, a4paper, dvipdfmx, latex]{article}\n'
     '\\usepackage{cite}\n'
@@ -20,16 +19,20 @@ class TeXWrite:
     def __init__(
             self,
             auxdir='.aux',
-            texname='wdbibtex.tex',
+            targetbasename='wdbibtex',
             texcmd='latex',
             texopts='-interaction=nonstopmode -file-line-error',
+            bibtexcmd='bibtex',
+            bibtexopts='',
             preamble=defaultpreamble,
             autostart=False):
         self.__cwd = pathlib.Path(os.getcwd())
         self.__auxdir = self.__cwd / auxdir
         self.__auxdir.mkdir(exist_ok=True)
-        self.__texname = texname
+        self.__targetbasename = targetbasename
         self.__texcmd = texcmd
+        self.__bibtexcmd = bibtexcmd
+        self.__bibtexopts = bibtexopts
         self.__texopts = texopts
         self.__preamble = preamble
 
@@ -38,7 +41,7 @@ class TeXWrite:
             self.compile()
 
     def write(self, contents):
-        with open(self.__auxdir / self.__texname, 'w') as f:
+        with open(self.__auxdir / (self.__targetbasename + '.tex'), 'w') as f:
             f.writelines(
                 self.__preamble + '\\begin{document}\n'
                 + contents + '\\end{document}\n'
@@ -51,7 +54,19 @@ class TeXWrite:
         cmd = ' '.join([
                 self.__texcmd,
                 self.__texopts,
-                str(self.__auxdir / self.__texname),
+                str(self.__auxdir / (self.__targetbasename + '.tex')),
+            ])
+        subprocess.call(cmd, shell=True)
+        os.chdir(cwd)
+
+    def bibtex(self):
+        import subprocess
+        cwd = os.getcwd()
+        os.chdir(self.__auxdir)
+        cmd = ' '.join([
+                self.__bibtexcmd,
+                self.__bibtexopts,
+                self.__targetbasename,
             ])
         subprocess.call(cmd, shell=True)
         os.chdir(cwd)
