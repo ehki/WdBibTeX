aeba77d9cb63eaf7f356b924f26f4ce3537b41a5
ehki <0y35.ejiri.vmqewyhw@gmail.com>
2022-02-19 11:02:42 +0900

    Prepare distribution

---
+++ wdbibtex/__init__.py
@@ -0,0 +1,6 @@
+__copyright__ = 'Copyright (C) 2022 Haruki EJIRI'
+__version__ = '0.0-dev'
+__license__ = 'MIT'
+__author__ = 'Haruki EJIRI'
+__author_email__ = '0y35.ejiri.vmqewyhw@gmail.com'
+__url__ = 'https://github.com/ehki/WdBibTeX'
