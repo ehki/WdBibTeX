eb6f40bd8f4268125024821e927ae6570f254b16
ehki <0y35.ejiri.vmqewyhw@gmail.com>
2022-02-23 14:43:26 +0900

    Add test_multiple_citations_compile_citation

--- wdbibtex/tests/tests.py
+++ wdbibtex/tests/tests.py
@@ -63,3 +63,42 @@ class TestLaTeX(unittest.TestCase):
         # Clear working directory
         shutil.rmtree('.tmp')
         os.chdir(cwd)
+
+    def test_multiple_citations_compile_citation(self):
+        cwd = os.getcwd()
+        exampledir = os.path.join(
+            os.path.dirname(__file__), '..', '..', 'examples'
+        )
+        os.chdir(os.path.join(exampledir, 'example2'))
+        ltx = wdbibtex.LaTeXHandler()
+        ltx.write(
+            ('Test contents with one citation \\cite{enArticle1}.\n'
+             'Another citation \\cite{enArticle2}.\n'
+             'Multiple citations in one citecommand '
+             '\\cite{enArticle1,enArticle3}\n'),
+            bibstyle='ieeetr')
+        ltx.compile()
+        # The .tex size will 288 B
+        self.assertEqual(os.path.getsize('.tmp/wdbib.tex'), 288)
+        # The .aux size will 220 B
+        self.assertEqual(os.path.getsize('.tmp/wdbib.aux'), 220)
+        # The .bbl size will 453 B
+        self.assertEqual(os.path.getsize('.tmp/wdbib.bbl'), 453)
+        with open('.tmp/wdbib.tex', 'r') as f:
+            print(''.join(f.readlines()))
+        with open('.tmp/wdbib.aux', 'r') as f:
+            print(''.join(f.readlines()))
+        with open('.tmp/wdbib.bbl', 'r') as f:
+            print(''.join(f.readlines()))
+
+        # Parse test for aux file.
+        ltx.parse_aux()
+        ltx.build_conversion_dict()
+        print(ltx.conversion_dict)
+        self.assertEqual(ltx.conversion_dict['enArticle1'], '1')
+        self.assertEqual(ltx.conversion_dict['enArticle2'], '2')
+        self.assertEqual(ltx.conversion_dict['enArticle1,enArticle3'], '1,3')
+
+        # Clear working directory
+        shutil.rmtree('.tmp')
+        os.chdir(cwd)
