558e5f98b6e8b4ed516720b940de79c1812ade76
ehki <0y35.ejiri.vmqewyhw@gmail.com>
2022-02-23 10:24:44 +0900

    Merge bbl.py into latex.py

--- wdbibtex/latex.py
+++ wdbibtex/latex.py
@@ -1,6 +1,7 @@
 import locale
 import pathlib
 import os
+import re
 
 
 class LaTeXHandler:
@@ -111,3 +112,34 @@ class LaTeXHandler:
         subprocess.call(latexcmd, shell=True)
         subprocess.call(latexcmd, shell=True)
         os.chdir(cwd)
+
+    def reference_text(self, replacer=None):
+        """Returns reference plain text to incert word file.
+        """
+        if replacer is None:
+            replacer = {}
+        replacer.update({
+            r'\n  ': ' ',
+            r'\{\\em (.*)\}': r'\1',
+            r'~': ' ',
+            r'--': u'\u2014',
+            r'``': '“',
+            r"''": '”',
+            r'\n\n': '\n'
+            })
+        thebib_begin = None
+        for i, line in enumerate(self.__bbldata):
+            if line.startswith('\\bibitem') and thebib_begin is None:
+                thebib_begin = i
+            if line.startswith('\\end{thebibliography}'):
+                thebib_end = i
+        ret_text = ''.join(self.__bbldata[thebib_begin: thebib_end])
+        for k, v in replacer.items():
+            ret_text = re.sub(k, v, ret_text)
+        return ret_text
+
+    def read_bbl(self):
+        """Read .bbl file.
+        """
+        with open(self.__auxdir / (self.__targetbasename + '.bbl'), 'r') as f:
+            self.__bbldata = f.readlines()
