c3d467d6e4095f2b52770d2d999289db47604743
ehki <0y35.ejiri.vmqewyhw@gmail.com>
2022-02-26 23:32:46 +0900

    Remove wdbibtex_cli and move to __main__

--- wdbibtex/__main__.py
+++ wdbibtex/__main__.py
@@ -1,8 +1,52 @@
 import sys
+import argparse
 
-from wdbibtex.wdbibtex_cli import main
+import wdbibtex
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='WdBibTeX is a BibTeX citation formatter for MS word.'
+    )
+    parser.add_argument(
+        'file',
+        type=str,
+        help=(
+            'File to BibTeX format.'
+        )
+    )
+    parser.add_argument(
+        '--bibstyle',
+        type=str,
+        default=None,
+        help=(
+            'BibTeX style file. '
+            'Default: .bst in target file directory'
+        )
+    )
+    parser.add_argument(
+        '--bibfile',
+        type=str,
+        default=None,
+        help=(
+            'Bibliography file. '
+            'Default: all .bib in target file directory'
+        )
+    )
+    parser.add_argument(
+        '--keeptexdir',
+        action='store_true',
+        help=(
+            'Keep LaTeX files and directory after run. '
+            'Default: False(= clean LaTeX files/directory)'
+        )
+    )
+    args = parser.parse_args()
+    wb = wdbibtex.WdBibTeX(args.file)
+    wb.build(bib=args.bibfile, bst=args.bibstyle)
+    wb.close(clear=not args.keeptmpdir)
+    return 0
 
 
 if __name__ == '__main__':
-    # Excecute with python -m wdbibtex
     sys.exit(main())
