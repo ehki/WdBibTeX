6865046bdd26ad87394bef6f42f2f8e04f8b5755
ehki <0y35.ejiri.vmqewyhw@gmail.com>
2022-02-27 21:40:38 +0900

    version 0.1

--- wdbibtex/__init__.py
+++ wdbibtex/__init__.py
@@ -7,7 +7,7 @@ __all__ = [
 ]
 
 __copyright__ = 'Copyright (C) 2022 Haruki EJIRI'
-__version__ = '0.0-dev'
+__version__ = '0.1'
 __license__ = 'MIT'
 __author__ = 'Haruki EJIRI'
 __author_email__ = '0y35.ejiri.vmqewyhw@gmail.com'
