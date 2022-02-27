e86903151171d083ff819b45bc94083af4e86d86
ehki <0y35.ejiri.vmqewyhw@gmail.com>
2022-02-27 20:52:52 +0900

    Add MANIFEST.in, set url,requirements

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', encoding='utf-8') as f:
 setup(
     name='wdbibtex',
     version=wdbibtex.__version__,
-    url='http://pypi.python.org/pypi/pyjjy/',
+    url='http://pypi.python.org/pypi/wdbibtex/',
     author='Haruki EJIRI',
     author_email='0y35.ejiri.vmqewyhw@gmail.com',
     description='WdBibTeX is a BibTeX toolkit for MS Word.',
@@ -17,14 +17,20 @@ setup(
     python_requires='>=3.6',
     packages=find_packages(),
     include_package_data=True,
+    install_requires=['pywin32>=302'],
     long_description=long_description,
     long_description_content_type='text/markdown',
+
+    project_urls={
+        'Document': 'https://ehki.github.io/WdBibTeX',
+        'Source': 'https://github.com/ehki/WdBibTeX',
+    },
 
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Operating System :: Microsoft'
+        'Operating System :: Microsoft',
         'Topic :: Text Editors :: Documentation',
         'Topic :: Text Editors :: Word Processors',
         'Topic :: Text Processing :: Markup :: LaTeX',
