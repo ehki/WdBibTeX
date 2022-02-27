ecf0cea380d2bf682813a51dc2cb5100388ebe3c
ehki <0y35.ejiri.vmqewyhw@gmail.com>
2022-02-27 11:38:07 +0900

    Revise test_multiple_citations_compile_citation

--- wdbibtex/tests/tests.py
+++ wdbibtex/tests/tests.py
@@ -170,45 +170,112 @@ class TestLaTeX(unittest.TestCase):
         os.chdir(cwd)
 
     def test_multiple_citations_compile_citation(self):
+        """Pass if multiple citations are collectly converted.
+        """
         cwd = os.getcwd()
         exampledir = os.path.join(
             os.path.dirname(__file__), '..', '..', 'examples'
         )
         os.chdir(os.path.join(exampledir, 'example2'))
-        ltx = wdbibtex.LaTeXHandler()
+
+        # Copy LaTeX bib and bst files to workdir
+        os.makedirs('.tmp', exist_ok=True)
+        for b in glob.glob('*.bib'):
+            shutil.copy(b, '.tmp/')
+
+        ltx = wdbibtex.LaTeX()
         ltx.write(
             ('Test contents with one citation \\cite{enArticle1}.\n'
              'Another citation \\cite{enArticle2}.\n'
              'Multiple citations in one citecommand '
              '\\cite{enArticle1,enArticle3}\n'),
-            bibstyle='ieeetr')
-        ltx.compile()
-        # The .tex size will 288 B
-        self.assertEqual(os.path.getsize('.tmp/wdbib.tex'), 288)
-        # The .aux size will 220 B
-        self.assertEqual(os.path.getsize('.tmp/wdbib.aux'), 220)
-        # The .bbl size will 453 B
-        self.assertEqual(os.path.getsize('.tmp/wdbib.bbl'), 453)
+            bst='ieeetr')
+        ltx.build()
+
+        # File check
+        correct = [
+            '\\documentclass[latex]{article}\n',
+            '\\usepackage{cite}\n',
+            '\\bibliographystyle{ieeetr}\n',
+            '\\begin{document}\n',
+            'Test contents with one citation \\cite{enArticle1}.\n',
+            'Another citation \\cite{enArticle2}.\n',
+            'Multiple citations in one citecommand \\cite{enArticle1,enArticle3}\n',
+            '\\bibliography{library}\n',
+            '\\end{document}\n',
+        ]
         with open('.tmp/wdbib.tex', 'r') as f:
-            print(''.join(f.readlines()))
+            contents = f.readlines()
+
+        for c1, c2 in itertools.zip_longest(correct, contents):
+            self.assertEqual(c1, c2)
+
+        # File check
+        correct = [
+            '\\relax \n',
+            '\\bibstyle{ieeetr}\n',
+            '\\citation{enArticle1}\n',
+            '\\citation{enArticle2}\n',
+            '\\citation{enArticle1,enArticle3}\n',
+            '\\bibdata{library}\n',
+            '\\bibcite{enArticle1}{1}\n',
+            '\\bibcite{enArticle2}{2}\n',
+            '\\bibcite{enArticle3}{3}\n',
+            '\\gdef \\@abspage@last{1}\n',
+        ]
         with open('.tmp/wdbib.aux', 'r') as f:
-            print(''.join(f.readlines()))
+            contents = f.readlines()
+
+        for c1, c2 in itertools.zip_longest(correct, contents):
+            self.assertEqual(c1, c2)
+
+        # File check
+        correct = [
+            "\\begin{thebibliography}{1}\n",
+            "\n",
+            "\\bibitem{enArticle1}\n",
+            "I.~Yamada, J.~Yamada, S.~Yamada, and S.~Yamada, ``Title1,'' {\em Japanese\n",
+            "  Journal}, vol.~15, pp.~20--30, march 2019.\n",
+            "\n",
+            "\\bibitem{enArticle2}\n",
+            "G.~Yamada and R.~Yamada, ``Title2,'' {\\em Japanese Journal}, vol.~15, p.~21,\n",
+            "  dec. 2019.\n",
+            "\n",
+            "\\bibitem{enArticle3}\n",
+            "G.~Yamada and R.~Yamada, ``Title2 is true?,'' {\\em IEEE Transactions on Pattern\n",
+            "  Analysis and Machine Intelligence}, nov 2018.\n",
+            "\n",
+            "\\end{thebibliography}\n",
+        ]
         with open('.tmp/wdbib.bbl', 'r') as f:
-            print(''.join(f.readlines()))
+            contents = f.readlines()
+
+        for c1, c2 in itertools.zip_longest(correct, contents):
+            self.assertEqual(c1, c2)
 
         # Parse test for aux file.
-        ltx.parse_aux()
-        ltx.build_conversion_dict()
-        print(ltx.conversion_dict)
-        self.assertEqual(ltx.conversion_dict['enArticle1'], '1')
-        self.assertEqual(ltx.conversion_dict['enArticle2'], '2')
-        self.assertEqual(ltx.conversion_dict['enArticle1,enArticle3'], '1,3')
-        ltx.read_bbl()
-        print(ltx.get_thebibliography_text())
-        self.assertEqual(len(ltx.get_thebibliography_text()), 318)
-        print(ltx.get_replacer())
-        self.assertEqual(len(str(ltx.get_replacer())), 463)
-
+        ltx.read_aux()
+        self.assertEqual(
+            ltx.cnd['\\\\cite\\{enArticle1\\}'],
+            '[1]'
+        )
+        self.assertEqual(
+            ltx.cnd['\\\\cite\\{enArticle2\\}'],
+            '[2]'
+        )
+        self.assertEqual(
+            ltx.cnd['\\\\cite\\{enArticle1,enArticle3\\}'],
+            '[1,3]'
+        )
+        self.assertEqual(
+            ltx.tbt,
+            (u"[1]\tI. Yamada, J. Yamada, S. Yamada, and S. Yamada, “Title1,” "
+             u"Japanese Journal, vol. 15, pp. 20—30, march 2019.\n"
+             u"[2]\tG. Yamada and R. Yamada, “Title2,” Japanese Journal, "
+             u"vol. 15, p. 21, dec. 2019.\n"
+             u"[3]\tG. Yamada and R. Yamada, “Title2 is true?,” IEEE Transactions "
+             u"on Pattern Analysis and Machine Intelligence, nov 2018.\n")
+        )
         # Clear working directory
         shutil.rmtree('.tmp')
         os.chdir(cwd)
