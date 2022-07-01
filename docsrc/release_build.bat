5dde0c8bd33a5416bd23492696ef25aa87a5cf66
ehki <0y35.ejiri.vmqewyhw@gmail.com>
2022-07-01 10:19:11 +0900

    Add build script

---
+++ docsrc/release_build.bat
@@ -0,0 +1,30 @@
+@REM Increment versions in
+@REM wdbibtex/__init__.py
+@REM docsrc/conf.py
+cd ../
+call %USERPROFILE%\Miniconda3\Scripts\activate.bat
+call conda remove -n wdbibtex-v0.2.2 --all -y
+call conda create -n wdbibtex-v0.2.2 python=3.9.6 -y
+call conda activate wdbibtex-v0.2.2
+call conda install pywin32 -y
+rm -rf dist build wdbibtex.egg-info debug.log
+python setup.py sdist
+python setup.py bdist_wheel
+pip install dist\wdbibtex-0.2.2-py3-none-any.whl
+pip install pytest
+python -m pytest
+pause
+@REM The following commands should be executed manually`
+@REM twine upload --repository testpypi dist/*
+@REM pause
+@REM start "" https://test.pypi.org/project/wdbibtex
+@REM pause
+@REM pip uninstall wdbibtex -y
+@REM pip --no-cache-dir install --index-url https://test.pypi.org/simple/ wdbibtex
+@REM twine upload --repository pypi dist/*
+@REM pause
+@REM start "" https://pypi.org/project/wdbibtex
+@REM pause
+@REM pip uninstall wdbibtex -y
+@REM pip --no-cache-dir install wdbibtex
+pause
